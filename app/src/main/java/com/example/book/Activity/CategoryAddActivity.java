7588a95df031c90e10a6034049726e983b59b944
TuanKenOfficial <nhatyeuthu2013@gmail.com>
2023-07-25 08:31:57 +0700

    Da xu ly duoc danh muc book trung nhau

--- app/src/main/java/com/example/book/Activity/CategoryAddActivity.java
+++ app/src/main/java/com/example/book/Activity/CategoryAddActivity.java
@@ -12,6 +12,7 @@ import android.view.View;
 import android.widget.Toast;
 
 import com.example.book.Adapter.AdapterReport;
+import com.example.book.Model.book.ModelCategory;
 import com.example.book.Model.report.ModelReport;
 import com.example.book.databinding.ActivityCategoryAddBinding;
 import com.google.android.gms.tasks.OnFailureListener;
@@ -24,6 +25,7 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.HashMap;
+import java.util.Objects;
 
 public class CategoryAddActivity extends AppCompatActivity {
 
@@ -37,13 +39,10 @@ public class CategoryAddActivity extends AppCompatActivity {
     private ProgressDialog progressDialog;
 
 
-
 //    private ArrayList<ModelCategory> categoryArrayList;
 
 
-
     private static final String TAG = "CATEGORY";
-
 
 
     @Override
@@ -76,10 +75,12 @@ public class CategoryAddActivity extends AppCompatActivity {
             }
         });
 
+
     }
 
 
     private String categorys = "";
+    private String id;
 
     //status
 //     private String status1 = "0";
@@ -89,15 +90,33 @@ public class CategoryAddActivity extends AppCompatActivity {
 
         //get data
         categorys = binding.edtCategory.getText().toString().trim();
-        if (TextUtils.isEmpty(categorys)){
+        if (TextUtils.isEmpty(categorys)) {
             Toast.makeText(this, "Vui lòng nhập danh mục sách...", Toast.LENGTH_SHORT).show();
         }
-
         else {
-            addCategoryFirebase();
+            //Xử lý danh sách trùng nhau
+            DatabaseReference categoryRef = FirebaseDatabase.getInstance().getReference("Categories");
+            categoryRef.orderByChild("category").equalTo(categorys).addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    if (snapshot.exists()) {
+                        //category already exists
+                        Toast.makeText(CategoryAddActivity.this, "Danh mục sách đã tồn tại.", Toast.LENGTH_SHORT).show();
+                    } else{
+                        //category does not exist, add to Firebase
+                        addCategoryFirebase();
+                    }
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+                    //error handling
+                    Toast.makeText(CategoryAddActivity.this, ""+databaseError.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            });
         }
     }
-
+//            addCategoryFirebase();
 
 
 
@@ -108,31 +127,30 @@ public class CategoryAddActivity extends AppCompatActivity {
 
 
         // id firebase tự sinh ra và ta lấy id đó xuống và lưu lại
-        String id =  FirebaseDatabase.getInstance().getReference().push().getKey();
+        String id = FirebaseDatabase.getInstance().getReference().push().getKey();
         //thời gian
         long timestamp = System.currentTimeMillis();
 
         //setup upload database
         HashMap<String, Object> hashMap = new HashMap<>();
-        hashMap.put("id", ""+id);
-        hashMap.put("category", "" +categorys);
+        hashMap.put("id", "" + id);
+        hashMap.put("category", ""+categorys);
         hashMap.put("timestamp", timestamp);
-        hashMap.put("uid", "" +firebaseAuth.getUid());
-//        hashMap.put("status", status);//status
+        hashMap.put("uid", "" + firebaseAuth.getUid());
 
-
-        //Đưa lên database.... Database Root > Categories > catogoryId > category info
         DatabaseReference ref = FirebaseDatabase.getInstance().getReference("Categories");
-        ref.child(""+id)
+        ref.child("" + id)
                 .setValue(hashMap)
                 .addOnSuccessListener(new OnSuccessListener<Void>() {
                     @Override
                     public void onSuccess(Void unused) {
+
                         //success
-                        Log.d(TAG, "onSuccess: Upload thành công: "+categorys);
+                        Log.d(TAG, "onSuccess: Upload thành công: " + categorys);
                         progressDialog.dismiss();
-                        Toast.makeText(CategoryAddActivity.this, "Danh mục: "+categorys+ " upload thành công", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(CategoryAddActivity.this, "Danh mục: " + categorys + " upload thành công", Toast.LENGTH_SHORT).show();
                         startActivity(new Intent(CategoryAddActivity.this, DashboardAdminActivity.class));
+
 
                     }
                 })

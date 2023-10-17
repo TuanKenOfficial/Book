61dbb70985e571cbb9a99b39d7d0cda47c4fdb97
TuanKenOfficial <nhatyeuthu2013@gmail.com>
2023-10-17 21:35:05 +0700

    update

--- app/src/main/java/com/example/book/Activity/DashboardUserActivity.java
+++ app/src/main/java/com/example/book/Activity/DashboardUserActivity.java
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentStatePagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
@@ -136,7 +137,9 @@ public class DashboardUserActivity extends AppCompatActivity {
 
             }
         });
+
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 switch (item.getItemId()){
